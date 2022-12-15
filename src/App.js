00f88e2bcae69613d830168a4e03085d8c3e91e6
Revan232 <revanyuda001@gmail.com>
2022-12-16 00:36:54 +0700

    tugas

---
+++ src/App.js
@@ -0,0 +1,28 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://cdn.pixabay.com/photo/2013/07/12/12/19/colors-145558_960_720.png" className="App-logo" alt="logo" />
+        <p>
+          Ini Adalah reactjs
+        </p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
